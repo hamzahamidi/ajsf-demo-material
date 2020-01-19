f5eca7d5e960d1011ee9a3bfda7ccf07a79c5f7f
Hamza HAMIDI <hamidihamzacg@gmail.com>
2020-01-19 14:56:05 +0100

    Feat(Global): Init ajsf material demo

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ajsf-demo app is running!');
+    expect(page.getTitleText()).toEqual('ajsf-demo-material app is running!');
   });
 
   afterEach(async () => {
